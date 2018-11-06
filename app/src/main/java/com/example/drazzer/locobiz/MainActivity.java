c379817911a2adfd681f482e352f1aac59762d01
Drazzer <Ashiq.racer@gmail.com>
2018-11-06 15:41:55 +0600

    start

--- app/src/main/java/com/example/drazzer/locobiz/MainActivity.java
+++ app/src/main/java/com/example/drazzer/locobiz/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.drazzer.locobiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.drazzer.locobiz.Map.MapsActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnMap= findViewById(R.id.btnmap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
