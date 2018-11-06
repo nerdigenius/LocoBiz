a3f24ea34cfe9e2889e3955d86f91659465aa592
Drazzer <Ashiq.racer@gmail.com>
2018-11-06 14:58:07 +0600

    start

--- app/src/main/java/com/example/drazzer/locobiz/Map/MapsActivity.java
+++ app/src/main/java/com/example/drazzer/locobiz/Map/MapsActivity.java
@@ -11,7 +11,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
 
     private GoogleMap mMap;
 
